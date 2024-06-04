07357f2c7226dfa7457327ba0d91e864baf937c3
Jungmin Lee <75019459+Lucky-kor@users.noreply.github.com>
2024-06-05 08:55:20 +0900

    Add files via upload

---
+++ src/main/java/com/springboot/coffee/CoffeeController.java
@@ -0,0 +1,30 @@
+package com.springboot.coffee;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/coffees")
+public class CoffeeController {
+    private final Map<Long, Map<String, Object>> coffees = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        Map<String, Object> coffee1 = new HashMap<>();
+        long coffeeId = 1L;
+        coffee1.put("coffeeId", coffeeId);
+        coffee1.put("korName", "바닐라 라떼");
+        coffee1.put("engName", "Vanilla Latte");
+        coffee1.put("price", 4500);
+
+        coffees.put(coffeeId, coffee1);
+    }
+
+    //---------------- 여기서 부터 아래에 코드를 구현하세요! -------------------//
+    // 1. 커피 정보 수정을 위한 핸들러 메서드 구현
+    // 2. 커피 정보 삭제를 위한 핸들러 서드 구현
+}
---
+++ src/main/java/com/springboot/member/MemberController.java
@@ -0,0 +1,31 @@
+package com.springboot.member;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v1/members")
+public class MemberController {
+    private final Map<Long, Map<String, Object>> members = new HashMap<>();
+
+    @PostConstruct
+    public void init() {
+        Map<String, Object> member1 = new HashMap<>();
+        long memberId = 1L;
+        member1.put("memberId", memberId);
+        member1.put("email", "hgd@gmail.com");
+        member1.put("name", "홍길동");
+        member1.put("phone", "010-1234-5678");
+
+        members.put(memberId, member1);
+    }
+
+    //---------------- 여기서 부터 아래에 코드를 구현하세요! --------------------//
+    // 1. 회원 정보 수정을 위한 핸들러 메서드 구현
+    // 2. 회원 정보 삭제를 위한 핸들러 메서드 구현
+
+}
